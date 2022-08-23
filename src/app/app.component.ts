8f63e38151c54bb070710449c472ff19c09eb981
am <am@hoxro.com>
2022-08-23 19:54:30 +0600

    game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,113 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'miniGame';
+export class AppComponent implements OnInit {
+  isGameStarted: boolean = false
+  isGameOver: boolean = false
+  currentLevel: any;
+  levels: any[] = [
+    {
+      label: 'Easy',
+      speed: 2000
+    },
+    {
+      label: 'Normal',
+      speed: 1000
+    },
+    {
+      label: 'Hard',
+      speed: 500
+    }
+  ];
+
+  screenHeight!: number;
+  screenWidth!: number;
+  containerHeight!: number;
+  containerWidth!: number;
+
+  top = 0;
+  left = 0;
+
+
+  timer: any;
+  countDown: number = 20;
+  score: number = 0
+  cd: any;
+
+
+  @ViewChild('container') container!: ElementRef;
+
+  constructor() {
+    this.currentLevel = 2000;
+  }
+
+  ngOnInit(): void {
+    this.getScreenSize();
+
+  }
+
+
+  changeLevel(level: any) {
+    this.currentLevel = level
+    console.log(level)
+  }
+
+
+
+
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?) {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+    console.log(this.screenHeight, this.screenWidth);
+  }
+
+  startGame() {
+    this.isGameStarted = true
+    this.timer = setInterval(() => {
+      this.top = Math.floor(Math.random() * 500) + 1;
+      this.left = Math.floor(Math.random() * 500) + 1;
+    }, this.currentLevel)
+    this.cd = setInterval(() => {
+      this.countDown--
+      if (this.countDown <= 0) {
+        setTimeout(() => {
+          clearTimeout(this.cd)
+          this.stopGame();
+          this.isGameOver = false;
+          this.isGameStarted = false
+        }, 0)
+      }
+    }, 1000)
+
+  }
+
+  stopGame() {
+    setTimeout(() => {
+      clearTimeout(this.timer)
+      this.score = 0;
+    }, 0);
+    setTimeout(() => {
+      clearTimeout(this.cd)
+      this.isGameOver = false;
+      this.isGameStarted = false
+    }, 0)
+  }
+
+  resetGame() {
+    this.stopGame();
+    this.currentLevel = 2000;
+    this.score = 0;
+  }
+
+  makeScore() {
+    if (this.isGameStarted) {
+      this.score++
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +21,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatRadioModule,
+    MatSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
